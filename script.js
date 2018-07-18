cc4f452ca0480b612da2087435eb350dac3b59fd
zeanort <armandwork@hotmail.co.uk>
2018-07-18 14:31:09 +0100

    new files

---
+++ script.js
@@ -0,0 +1,14 @@
+// Token // 
+var token = "8d40593c3cc012af4b0fc67eba712ec8";
+
+// URLs // 
+
+var url = ; 
+var imgUrl = "https://image.tmdb.org/t/p/original" 
+var imgSrc ;
+
+
+// Output field // 
+
+var getMovie = document.querySelector();
+var movieGif = document.querySelector();

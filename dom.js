5709e8de83b6bf3c212f1c98302614951f144a7b
zeanort <armandwork@hotmail.co.uk>
2018-07-20 11:30:08 +0100

    removed unused variable and other fixes

--- dom.js
+++ dom.js
@@ -1,7 +1,6 @@
 var url =
     "https://api.themoviedb.org/3/discover/movie?api_key=8d40593c3cc012af4b0fc67eba712ec8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=";
 var imgUrl = "https://image.tmdb.org/t/p/original";
-var imgSrc;
 
 // Output/Input fields //
 var submit = document.querySelector("#submit-year");
